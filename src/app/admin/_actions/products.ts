32b1dc4d0301f51ab633f7ab99e84a541f0f7416
EricLam404 <lameric9888@gmail.com>
2024-04-21 12:13:58 -0400

    Add product interactability

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,7 +3,7 @@
 import db from "@/db/db"
 import { z } from "zod"
 import fs from "fs/promises"
-import { redirect } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -32,16 +32,36 @@ const addProduct = async (prevState:unknown, formData: FormData) => {
     const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
     await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
 
-    db.product.create({ data: {
-        isAvailableForPurchase: false,
-        name: data.name,
-        description: data.description,
-        priceInCents: data.priceInCents,
-        filePath: filePath,
-        imagePath: imagePath
-    }})
+    await db.product.create({
+        data: {
+          isAvailableForPurchase: false,
+          name: data.name,
+          description: data.description,
+          priceInCents: data.priceInCents,
+          filePath,
+          imagePath,
+        },
+    })
 
     redirect("/admin/products")
 }
 
 export default addProduct
+
+export async function toggleProductAvailablitiy(id: string, isAvailableForPurchase: boolean) {
+    await db.product.update({
+        where: { id }, data: {
+            isAvailableForPurchase
+        }
+    })
+}
+
+export async function deleteProduct(id: string) {
+    const product = await db.product.delete({
+        where: { id }
+    })
+    if (product == null) return notFound()
+
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
+}
