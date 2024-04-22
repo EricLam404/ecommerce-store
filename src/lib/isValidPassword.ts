4cb7baa548a8f16f9a524901e93524661d8ef333
EricLam404 <lameric9888@gmail.com>
2024-04-21 20:00:21 -0400

    Remove console.log

--- src/lib/isValidPassword.ts
+++ src/lib/isValidPassword.ts
@@ -10,6 +10,5 @@ async function hashPassword(password: string) {
     "SHA-512",
     new TextEncoder().encode(password)
     )
-    console.log(Buffer.from(arrayBuffer).toString("base64"))
     return Buffer.from(arrayBuffer).toString("base64")
 }
